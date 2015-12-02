5ccb416764ec983e10e224cb7a8d2f8a25ca3bf8
auyanik <ahmet.uyanik.dev@gmail.com>
2015-12-02 17:25:53 +0200

    log4j set up

--- src/main/java/com/project/service/PersistenceService.java
+++ src/main/java/com/project/service/PersistenceService.java
@@ -1,14 +1,13 @@
 package com.project.service;
 
-import com.mongodb.gridfs.GridFSDBFile;
 import com.project.interfaces.Crud;
 import com.project.model.Shop;
+import org.apache.log4j.Logger;
 import org.springframework.data.mongodb.core.MongoOperations;
-import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.gridfs.GridFsOperations;
-import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 
+
+import javax.annotation.PostConstruct;
 import java.io.InputStream;
 import java.util.List;
 
@@ -16,23 +15,35 @@ public class PersistenceService implements Crud {
 
     private MongoOperations mongoOperations;
 
+    public static Logger log;
+
+    @PostConstruct
+    public void init(){
+        log = Logger.getLogger(PersistenceService.class);
+        log.info("#--- Application Persistence Service initialize ");
+    }
+
     @Override
     public void create(Shop obj) {
+        log.info("#--- Creating new shopping instance");
         mongoOperations.insert(obj);
     }
 
     @Override
     public void delete(Shop obj) {
+        log.info("#--- Deleting shopping instance with name "+obj.getName());
         mongoOperations.remove(obj);
     }
 
     @Override
     public void update(Shop obj) {
+        log.info("#--- Saving shopping instance with name "+obj.getName());
         mongoOperations.save(obj);
     }
 
     @Override
     public Shop read(String id, Class cls) {
+        log.info("#--- Fetching shopping instance by id "+id);
         return  (Shop)mongoOperations.findById(id,cls);
     }
 
@@ -42,6 +53,7 @@ public class PersistenceService implements Crud {
 
     @Override
     public List readAll(Class cls) {
+        log.info("#--- Fetching all instance of document class "+cls.getName());
         return mongoOperations.findAll(cls);
     }
 
